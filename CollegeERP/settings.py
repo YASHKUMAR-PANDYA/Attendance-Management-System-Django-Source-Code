a1ddc715dfe7ad7aeb13c1ccf59db1582f369fae
YASHKUMAR-PANDYA <yashodhar0027@gmail.com>
2022-05-02 12:19:57 +0530

    test 1.1

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'CollegeERP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
-
